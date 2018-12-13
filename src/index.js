601984682ae7a6a52735f74d0ff821711a81123e
Phạm Ngọc Chiến <pnchien.bk@gmail.com>
2018-12-13 09:30:56 +0700

    form validate

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css"
-import App from './App';
 
+import {createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import rootReducer from './rootReducer'
+
+const store = createStore(
+    rootReducer, composeWithDevTools(applyMiddleware(thunk))
+)
 ReactDOM.render(
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>,
     document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
